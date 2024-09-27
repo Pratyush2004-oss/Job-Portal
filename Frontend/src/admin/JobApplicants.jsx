1d1f71005c3188218ae1496e1ddcc8b5916b23e6
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-09-27 16:08:42 +0530

    Job Board

--- Frontend/src/admin/JobApplicants.jsx
+++ Frontend/src/admin/JobApplicants.jsx
@@ -1,5 +1,5 @@
 import Navbar from '@/components/shared/Navbar'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ApplicantsTable from './components/ApplicantsTable'
 import axios from 'axios'
 import { APPLICATION_API_END_POINT } from '@/utils/constant'
@@ -9,12 +9,14 @@ import { setAllApplicants } from '@/redux/applicantsSlice'
 
 const JobApplicants = () => {
     const params = useParams();
+    const [loading, setLoading] = useState(false);
     const jobId = params.id;
     const dispatch = useDispatch();
     const { applicants } = useSelector(store => store.application)
     useEffect(() => {
         const fetchAllApplicants = async () => {
             try {
+                setLoading(true);
                 const res = await axios.get(`${APPLICATION_API_END_POINT}/${jobId}/applicants`, { withCredentials: true })
                 if (res.data.success) {
                     dispatch(setAllApplicants(res.data.job))
@@ -22,17 +24,27 @@ const JobApplicants = () => {
             } catch (error) {
                 console.log(error)
             }
+            finally {
+                setLoading(false)
+            }
         }
         fetchAllApplicants();
-    }, [])
-    return applicants && (
+    }, [jobId])
+    return (
         <div>
             <Navbar />
             <div className='p-3 mx-auto my-10 max-w-7xl'>
-                <h1 className='my-3 text-3xl font-bold '>Applicants ({applicants.applications.length})</h1>
-                <ApplicantsTable applicants = {applicants} />
+                {loading &&
+                    <div className='animate-ping'>
+                        <div className='bg-slate-400'></div>
+                    </div>}
+                {!loading && applicants.applications.length > 0 &&
+                    <>
+                        <h1 className='my-3 text-3xl font-bold '>Applicants ({applicants.applications.length})</h1>
+                        <ApplicantsTable applicants={applicants} />
+                    </>
+                }
             </div>
-            
         </div>
     )
 }
--- Frontend/src/auth/Login.jsx
+++ Frontend/src/auth/Login.jsx
@@ -58,23 +58,23 @@ const Login = () => {
     return (
         <div>
             <Navbar />
-            <div className='p-5 flex items-center justify-center max-w-7xl mx-auto'>
-                <form onSubmit={handleSubmit} className='md:w-1/2 border-gray-200 rounded-lg p-4 my-10 shadow-md'>
-                    <h1 className='font-bold text-center text-xl mb-5'>Log In</h1>
+            <div className='flex items-center justify-center p-5 mx-auto max-w-7xl'>
+                <form onSubmit={handleSubmit} className='p-4 my-10 border-gray-200 rounded-lg shadow-md md:w-1/2'>
+                    <h1 className='mb-5 text-xl font-bold text-center'>Log In</h1>
                     {/* Email */}
-                    <div className='gap-2 my-3 flex flex-col'>
+                    <div className='flex flex-col gap-2 my-3'>
                         <Label>E mail</Label>
                         <Input value={input.fullname} name='email' onChange={changeEventHandler} type='email' placeholder='Enter your email' />
                     </div>
                     {/* Password */}
-                    <div className='gap-2 my-3 flex flex-col'>
+                    <div className='flex flex-col gap-2 my-3'>
                         <Label>Password</Label>
-                        <Input value={input.password} name='password' onChange={changeEventHandler} type='password' placeholder='Enter your name' />
+                        <Input value={input.password} name='password' onChange={changeEventHandler} type='password' placeholder='Enter password' />
                     </div>
                     {/* Job type */}
                     <div className='flex flex-col my-3'>
                         <Label>Type</Label>
-                        <RadioGroup className='flex gap-5 items-center p-2'>
+                        <RadioGroup className='flex items-center gap-5 p-2'>
                             <div className="flex items-center space-x-2">
                                 <Input
                                     checked={input.role === 'job seekers'}
@@ -99,7 +99,7 @@ const Login = () => {
                             </div>
                         </RadioGroup>
                     </div>
-                    <Button type='submit' disabled={loading} className='w-full my-3'>{loading ? <LoaderPinwheel className='mr-2 w-4 h-4 animate-spin' /> : 'Submit'}</Button>
+                    <Button type='submit' disabled={loading} className='w-full my-3'>{loading ? <LoaderPinwheel className='w-4 h-4 mr-2 animate-spin' /> : 'Submit'}</Button>
                     <span className='text-sm'>Didn't have an account <Link to={'/signup'}><span className='font-bold text-green-600'>Signup</span></Link></span>
 
                 </form>
